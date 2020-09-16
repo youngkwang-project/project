749ca8d6891c4206aeb1852f12262afaf2c6395e
HKEDU <HKEDU@192.168.4.105>
2020-09-16 15:05:33 +0900

    Service 업데이트

--- Project/src/main/java/com/hk/project/service/MemberService.java
+++ Project/src/main/java/com/hk/project/service/MemberService.java
@@ -1,5 +1,36 @@
 package com.hk.project.service;
 
-public class MemberService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hk.project.mapper.MemberMapper;
+
+@Service
+public class MemberService {
+	
+	
+	@Autowired
+	MemberMapper memberMapper;
+	
+	public List<Member> memberListGet(){
+		return memberMapper.memberListGet();
+	}
+	
+	public int memberRegisterPost(Member member) {
+		return memberMapper.memberRegisterPost(member);
+	}
+	
+	public Member memberGetOne(int mno) {
+		return memberMapper.memberGetOne(mno);
+	}
+	
+	public int memberUpdatePost(Member member) {
+		return memberMapper.memberUpdatePost(member);
+	}
+	
+	public int memberDeletePost(int mno) {
+		return memberMapper.memberDeletePost(mno);
+	}
 }
