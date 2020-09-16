bea152bbf1fc0bc2f4125dccf9290f693bc8aa1c
hk_edu <hk_edu@192.168.4.112>
2020-09-16 10:31:06 +0900

    VO,Service_세환

--- Project/src/main/java/com/hk/project/service/MemberService.java
+++ Project/src/main/java/com/hk/project/service/MemberService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hk.project.mapper.MemberMapper;
 import com.hk.project.vo.Member;
 
 @Service
@@ -15,6 +16,21 @@ public class MemberService {
 	
 		public List<Member> memberList(){
 			return memberMapper.memberList();
+		}
+		
+		public Member memberRegister(Member member) {
+			return memberMapper.memberRegister(member);
+		}
+		
+		public Member memberUpdate(int mno) {
+			return memberMapper.memberUpdate(mno);
+		}
+		public int memberUpdateDone(Member member) {
+			return memberMapper.memberUpdateDone(member);
+		}
+		
+		public Member memberDelete(int mno) {
+			return memberMapper.memberDelete(mno);
 		}
 }
 	
